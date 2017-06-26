0a13d1f9e4e6a2257bb35e7f5deb71683b333f5a
Ihor <ihor1337@gmail.com>
2017-06-26 00:09:18 -0700

    bugfixes, dev-server settings

--- app/components/settings/settings.controller.js
+++ app/components/settings/settings.controller.js
@@ -3,5 +3,14 @@
  */
 
 export default class SettingsController{
+  constructor ($window){
+    "ngInject";
+    this._$window = $window;
+  }
 
+  //The close button method.
+  // Could be also a redirect to main page instead of going back.
+  goBack(){
+    this._$window.history.back();
+  }
 }
